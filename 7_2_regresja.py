24c38b91bdff7cc46489a1375a0d0b9ac99c1fa2
Pawel <pdrozda@op.pl>
2023-02-18 11:40:54 +0100

    add SVR linear, Random Forests regression model

--- 7_2_regresja.py
+++ 7_2_regresja.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sklearn.linear_model as sl
 
+
 # mamy 3 zmienne objaśniające x1,x2,x3 i zmienną objaśnianą y
 x = [[1, 2, 3], [5, 1, 5], [15, 2, 7], [25, 5, 5], [35, 11, 13], [45, 15, 12], [55, 34, 45], [60, 35, 12]]
 y = [4, 5, 20, 14, 32, 22, 38, 43]
@@ -22,3 +23,21 @@ print(y_prognoza.round(2))
 
 y_prognoza_1 = model_regresji.predict([[4, 8, 20]])
 print(y_prognoza_1.round(2))
+
+from sklearn.ensemble import RandomForestRegressor
+model_regresji_rf = RandomForestRegressor()
+model_regresji_rf.fit(x, y)
+print('Istotność poszczególnych zmiennych x1, x2, x3',model_regresji_rf.feature_importances_)
+R_sq_rf = model_regresji_rf.score(x, y)
+print('Współczynnik determinacji:', R_sq_rf)
+y_prognoza_rf = model_regresji_rf.predict(x)
+print('Prognozy Random Forest', y_prognoza_rf.round(2))
+
+from sklearn.svm import LinearSVR
+model_regresji_svr = LinearSVR()
+model_regresji_svr.fit(x, y)
+print('współczynniki modelu:', model_regresji_svr.coef_)
+R_sq_svr = model_regresji_svr.score(x, y)
+print('Współczynnik determinacji:', R_sq_svr)
+y_prognoza_svr = model_regresji_svr.predict(x)
+print(y_prognoza_svr)
